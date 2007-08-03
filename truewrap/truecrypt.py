c1a85b1abe30bff0528e1871ef25bb3a1e845570
audax <audax>
2007-08-03 16:08:18 +0000

    License update

--- truewrap/truecrypt.py
+++ truewrap/truecrypt.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python 
-#    truecrypt.py - A wrapper for TrueCrypt, see http://www.truecrypt.org/ for more information about it
 #
-#    Copyright (C) 2007  Jens Kadenbach
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#      gTrueCrypt.py - gTrueCrypt 0.0.1
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#   author:      Jens Kadenbach
+#   copyright:   2007 by Jens Kadenbach
+#   license:      GNU General Public License, version 2 only
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  This program is free software. It's allowed to modify and/or redistribute
+#  it under the terms of the GNU General Public License, published by the
+#  Free Software Foundation, version 2 only.
+#  You should have received a copy of this license along with this software.
+#  If not, see http://www.gnu.org/licenses/!
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 # 
+
 
 import os
 import sys
