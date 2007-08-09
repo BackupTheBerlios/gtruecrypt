47a8d976430608b20e03c341a9634dad6db902f8
audax <audax>
2007-08-09 11:24:43 +0000

    new messages included

--- truewrap/tcerr.py
+++ truewrap/tcerr.py
@@ -1,4 +1,14 @@
 # Error messages for the truecrypt classes
 
-missing_password = "nopass"
+no_target = "no mount target given"
+umounted = "volume not mounted"
+allready_mounted = "volume allready mounted"
+mounted = "volume mounted"
+unknown_error = "error unknown"
+created = "volume created"
+not_found = "volume not found"
+missing_password = "no password given"
+missing_sudo = "sudo password illegal or missing"
 cannot_create_dir = "cannot create dir"
+voltype = "illegal volume type"
+filesystem = "illegal filesystem"
