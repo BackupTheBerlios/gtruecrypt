3c53af14ee75b5523a477633046236c5a65acc7b
stein <stein>
2007-10-23 07:06:52 +0000

    Changed the dialogs to "real" dialogs; Added the "add"-function (Ok, it's only a popup-dialog without any features); Added the SysTray: If you click on the systray-Icon or close the window, the window will hide. On the next click, it will appear...

--- truewrap/gTrueCrypt.py
+++ truewrap/gTrueCrypt.py
@@ -4,7 +4,7 @@
 #
 #	author:		Sebastian Billaudelle
 #	copyright:	2007 by Sebastian Billaudelle
-#	license:		GNU General Public License, version 2 only
+#	license:	GNU General Public License, version 2 only
 #
 #
 #	This program is free software. It's allowed to modify and/or redistribute
@@ -36,17 +36,21 @@ except:
 """This is the main class of gTrueCrypt"""
 class gTrueCrypt:
 	def __init__(self):
+		self._icon_path = os.path.join(os.path.dirname(__file__), "gTrueCrypt.png")
+		self._icon = gtk.gdk.pixbuf_new_from_file(self._icon_path)
 		"""Get list from backend"""
 		self.TW = TrueCrypt()
 		self.__container = self.TW.getList()
 		self.createMainWindow()
 		self.setMainWindowLayout()
 		self.loadList()
+		self._show_hide_counter = 1
 
 	def createMainWindow(self):
 		"""Create a new window and show it"""
 		self.main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.main_window.set_title("gTrueCrypt")
+		self.main_window.set_icon(self._icon)
 		self.main_window.set_border_width(20)
 		self.main_window.set_size_request(650, 200)
 		self.main_window.connect("delete_event", self.delete_event)
@@ -86,16 +90,16 @@ class gTrueCrypt:
 		self.edit_button.connect('clicked', self.edit)
 		self.add_button = gtk.Button("add")
 		self.add_button.show()
-		#self.add_button.connect('clicked', self.add)
-		self.pref_button = gtk.Button("preferences")
-		self.pref_button.show()
+		self.add_button.connect('clicked', self.add)
+		#self.pref_button = gtk.Button("preferences")
+		#self.pref_button.show()
 		#self.pref_button.connect('clicked', self.pref)
 		self.hbox = gtk.HBox()
 		self.hbox.show()
 		self.hbox.pack_start(self.action_button, True)
 		self.hbox.pack_start(self.edit_button, True)
 		self.hbox.pack_start(self.add_button, True)
-		self.hbox.pack_start(self.pref_button, True)
+		#self.hbox.pack_start(self.pref_button, True)
 		self.vbox = gtk.VBox()
 		self.vbox.show()
 		self.vbox.pack_start(self.scrolledwindow, True)
@@ -157,11 +161,19 @@ class gTrueCrypt:
 			self._state = self.list.get(self.iter, 3)
 			self.editDialog()
 
+	def add(self, button):
+		self.add_dialog_counter = 1
+		self.addDialog()
+
 	def editDialog(self, wrong_edit=False):
+		self._show_hide_counter = 2
 		"""Stuff for the password-dialog"""
-		self.edit_dialog_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		self.edit_dialog_window.set_title("Edit")
+		self.edit_dialog_window = gtk.Dialog("Edit", self.main_window,
+            gtk.DIALOG_NO_SEPARATOR,
+            ("Cancel",  gtk.RESPONSE_CLOSE,
+             "OK",         gtk.RESPONSE_OK))
 		self.edit_dialog_window.set_border_width(5)
+		self.edit_dialog_window.set_modal(True)
 		self.edit_dialog_layout = gtk.Table(4, 3, False)
 		self.edit_dialog_layout.set_row_spacings(2)
 		self.edit_dialog_layout.set_col_spacings(2)
@@ -176,37 +188,96 @@ class gTrueCrypt:
 		self.edit_dialog_path_entry.set_text(self._path[0])
 		self.edit_dialog_target_label = gtk.Label("Target")
 		self.edit_dialog_target_label.show()
+		self.edit_dialog_target_select = gtk.Button("...")
+		self.edit_dialog_target_select.connect('clicked', self.targetFileselector)
+		self.edit_dialog_target_select.show()
 		self.edit_dialog_target_entry = gtk.Entry(max=0)
 		self.edit_dialog_target_entry.set_text(self._target[0])
 		self.edit_dialog_label.show()
 		self.edit_dialog_path_entry.show()
-		self.edit_dialog_button_ok = gtk.Button("OK")
-		self.edit_dialog_button_cancel = gtk.Button("Cancel")
-		self.edit_dialog_button_ok.connect('clicked', self.editDialogOk)
-		self.edit_dialog_button_cancel.connect('clicked', self.editDialogCancel)
 		self.edit_dialog_layout.attach(self.edit_dialog_label, 0, 2, 0, 1)
   		self.edit_dialog_layout.attach(self.edit_dialog_path_label, 0, 1, 1, 2)
   		self.edit_dialog_layout.attach(self.edit_dialog_path_entry, 1, 2, 1, 2)
   		self.edit_dialog_layout.attach(self.edit_dialog_path_select, 2, 3, 1, 2)
   		self.edit_dialog_layout.attach(self.edit_dialog_target_label, 0, 1, 2, 3)
   		self.edit_dialog_layout.attach(self.edit_dialog_target_entry, 1, 2, 2, 3)
-  		self.edit_dialog_layout.attach(self.edit_dialog_button_ok, 0, 1, 3, 4)
-  		self.edit_dialog_layout.attach(self.edit_dialog_button_cancel, 1, 2, 3, 4)
-		self.edit_dialog_window.add(self.edit_dialog_layout)
+  		self.edit_dialog_layout.attach(self.edit_dialog_target_select, 2, 3, 2, 3)
+		self.edit_dialog_window.vbox.pack_start(self.edit_dialog_layout, True, True, 0)
 		self.edit_dialog_window.show_all()
+		self.edit_dialog_window.connect("response", self.responseEditDialog)
+
+	def addDialog(self):
+		if self.add_dialog_counter == 1:
+			self._show_hide_counter = 2
+			"""Stuff for the add-dialog"""
+			self.add_dialog_window = gtk.Dialog("Edit", self.main_window,
+				gtk.DIALOG_NO_SEPARATOR,
+            	("Cancel",  gtk.RESPONSE_CLOSE,
+             	"Forward",         gtk.RESPONSE_OK))
+			self.add_dialog_window.set_border_width(5)
+			self.add_dialog_window.set_modal(True)
+			self.add_dialog_label = gtk.Label("Here you can add new containers to the list and create new ones.")
+			self.add_dialog_label.show()
+			self.add_dialog_add_radio = gtk.RadioButton(None, "Add existing container to he list")
+			self.add_dialog_add_radio.show()
+			self.add_dialog_create_radio = gtk.RadioButton(self.add_dialog_add_radio, "Create new container")
+			self.add_dialog_create_radio.show()
+			self.add_dialog_create_radio.set_active(True)
+			self.add_dialog_window.vbox.pack_start(self.add_dialog_label, True, True, 0)
+			self.add_dialog_window.vbox.pack_start(self.add_dialog_create_radio, True, True, 0)
+			self.add_dialog_window.vbox.pack_start(self.add_dialog_add_radio, True, True, 0)
+			self.add_dialog_window.show_all()
+			self.add_dialog_window.connect("response", self.responseAddDialog)
+		elif self.add_dialog_counter == 2:
+			if self.add_dialog_add_radio.get_active() == True:
+				self.add_dialog_label.set_text("Select a container! It will be added to the list...")
+			elif self.add_dialog_create_radio.get_active() == True:
+				print "Not implemented yet!"
+
+
+	def responseEditDialog(self, dialog, response_id):
+		if response_id == gtk.RESPONSE_OK:
+			self.TW.setList(self._number[0], self.edit_dialog_path_entry.get_text(), self.edit_dialog_target_entry.get_text())
+			self.list.set(self.iter, 1, self.edit_dialog_path_entry.get_text(), 2, self.edit_dialog_target_entry.get_text())
+			self.edit_dialog_window.destroy()
+			self._show_hide_counter = 1
+		else:
+			dialog.destroy()
+			self._show_hide_counter = 1
+
+	def responseAddDialog(self, dialog, response_id):
+		if response_id == gtk.RESPONSE_OK:
+			self.add_dialog_counter = self.add_dialog_counter + 1
+			self.addDialog()
+		else:
+			dialog.destroy()
+			self._show_hide_counter = 1
 
 	def pathFileselector(self, button):
-		self.path_fileselector = gtk.FileSelection("select container")
-		self.path_fileselector.ok_button.connect("clicked", self.fileselectorOk)
-		self.path_fileselector.cancel_button.connect("clicked", self.fileselectorCancel)
+		self.path_fileselector = gtk.FileChooserDialog("Choose container", self.edit_dialog_window, gtk.FILE_CHOOSER_ACTION_OPEN, (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
 		self.path_fileselector.set_filename(self._path[0])
-		self.path_fileselector.show()
+		self.path_fileselector_response = self.path_fileselector.run()
+		if self.path_fileselector_response == gtk.RESPONSE_OK:
+			self.edit_dialog_path_entry.set_text(self.path_fileselector.get_filename())
+		self.path_fileselector.destroy()
+
+	def targetFileselector(self, button):
+		self.target_fileselector = gtk.FileChooserDialog("Choose target", None, gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER, (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+		self.target_fileselector.set_filename(self._target[0])
+		self.target_fileselector_response = self.target_fileselector.run()
+		if self.target_fileselector_response == gtk.RESPONSE_OK:
+			self.edit_dialog_target_entry.set_text(self.target_fileselector.get_filename())
+		self.target_fileselector.destroy()
 
 	def pwdDialog(self, wrong_pwd=False):
+		self._show_hide_counter = 2
 		"""Stuff for the password-dialog"""
-		self.pwd_dialog_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		self.pwd_dialog_window.set_title("Password")
+		self.pwd_dialog_window = gtk.Dialog("Enter password", self.main_window,
+            gtk.DIALOG_NO_SEPARATOR,
+            ("Cancel",  gtk.RESPONSE_CLOSE,
+             "OK",         gtk.RESPONSE_OK))
 		self.pwd_dialog_window.set_border_width(5)
+		self.pwd_dialog_window.set_modal(True)
 		self.pwd_dialog_layout = gtk.Table(3, 2, True)
 		self.pwd_dialog_layout.set_row_spacings(2)
 		self.pwd_dialog_layout.set_col_spacings(2)
@@ -217,16 +288,27 @@ class gTrueCrypt:
 		self.pwd_dialog_entry.set_visibility(False)
 		self.pwd_dialog_label.show()
 		self.pwd_dialog_entry.show()
-		self.pwd_dialog_button_ok = gtk.Button("OK")
-		self.pwd_dialog_button_cancel = gtk.Button("Cancel")
-		self.pwd_dialog_button_ok.connect('clicked', self.pwdDialogOk)
-		self.pwd_dialog_button_cancel.connect('clicked', self.pwdDialogCancel)
 		self.pwd_dialog_layout.attach(self.pwd_dialog_label, 0, 2, 0, 1)
   		self.pwd_dialog_layout.attach(self.pwd_dialog_entry, 0, 2, 1, 2)
-  		self.pwd_dialog_layout.attach(self.pwd_dialog_button_ok, 0, 1, 2, 3)
-  		self.pwd_dialog_layout.attach(self.pwd_dialog_button_cancel, 1, 2, 2, 3)
-		self.pwd_dialog_window.add(self.pwd_dialog_layout)
+		self.pwd_dialog_window.vbox.pack_start(self.pwd_dialog_layout, True, True, 0)
 		self.pwd_dialog_window.show_all()
+		self.pwd_dialog_window.connect("response", self.responsePwdDialog)
+
+	def responsePwdDialog(self, dialog, response_id):
+		if response_id == gtk.RESPONSE_OK:
+			self.pwd = self.pwd_dialog_entry.get_text()
+			self.result = self.TW.mount(self.number[0], self.target[0], self.pwd)
+			self.pwd = ""
+			self.pwd_dialog_window.destroy()
+			self._show_hide_counter = 1
+			if self.result == "wrong password": self.pwdDialog(True)
+			elif self.result == "no password given": self.pwdDialog(True)
+			elif self.result == "mounted":
+				self.list.set(self.iter, 3, "mounted")
+				self.action_button.set_label("unmount")
+		else:
+			dialog.destroy()
+			self._show_hide_counter = 1
 
 	def mountContainer(self, number, target):
 		"""Function for mounting a container"""
@@ -238,61 +320,54 @@ class gTrueCrypt:
 		self.action_button.set_label("mount")
 		self.TW.close(self.number[0])
 
-	def fileselectorOk(self, button):
-		"""Eventhandler of the OK-button in the"""
-		self._new_path = self.path_fileselector.get_filename()
-		self.edit_dialog_path_entry.set_text(self._new_path)
-		self.path_fileselector.destroy()
-
-	def fileselectorCancel(self, button):
-		"""Eventhandler of the Cancel-button in the"""
-		self.path_fileselector.destroy()
-
-	def editDialogOk(self, button):
-		"""Eventhandler of the OK-button in the editDialog"""
-		self.TW.setList(self._number[0], self.edit_dialog_path_entry.get_text(), self.edit_dialog_target_entry.get_text())
-		self.list.set(self.iter, 1, self.edit_dialog_path_entry.get_text(), 2, self.edit_dialog_target_entry.get_text())
-		self.edit_dialog_window.destroy()
-
-	def editDialogCancel(self, button):
-		"""Eventhandler of the Cancel-button in the editDialog"""
-		self.edit_dialog_window.destroy()
-
-	def pwdDialogOk(self, button):
-		"""Eventhandler of the OK-button in the pwdDialog"""
-		self.pwd = self.pwd_dialog_entry.get_text()
-		self.result = self.TW.mount(self.number[0], self.target[0], self.pwd)
-		self.pwd = ""
-		self.pwd_dialog_window.destroy()
-		if self.result == "wrong password": self.pwdDialog(True)
-		elif self.result == "no password given": self.pwdDialog(True)
-		elif self.result == "mounted":
-			self.list.set(self.iter, 3, "mounted")
-			self.action_button.set_label("unmount")
-
-	def pwdDialogCancel(self, button):
-		"""Eventhandler of the Cancel-button in the pwdDialog"""
-		self.pwd_dialog_window.destroy()
-
 	def delete_event(self, widget, event, data=None):
 		"""Stuff for closing gTC"""
+		self.main_window.hide_all()
+		self._show_hide_counter = 0
+		return True
+
+	def statusIcon(self):
+		self.icon = gtk.status_icon_new_from_file(self._icon_path) #TODO: Change icon
+		self.icon.connect('popup-menu', self.on_right_click)
+		self.icon.connect('activate', self.show_hide)
+
+	def on_right_click(self, icon, event_button, event_time):
+		self.statusIconMenu(event_button, event_time, icon)
+
+	def statusIconMenu(self, event_button, event_time, icon):
+		menu = gtk.Menu()
+		item = gtk.MenuItem("Close")
+		item.connect_object("activate", self.exit, menu)
+		item.show()
+		menu.append(item)
+		menu.popup(None, None, gtk.status_icon_position_menu, event_button, event_time, icon)
+
+	def exit(self, button):
 		gtk.main_quit()
-		return False
+
+	def show_hide(self, button):
+		if self._show_hide_counter == 1:
+			self.main_window.hide_all()
+			self._show_hide_counter = 0
+		elif self._show_hide_counter == 0:
+			self.main_window.show_all()
+			self._show_hide_counter = 1
 
 def main():
-        parser = OptionParser(version="%prog blub" )
-        (options, args) = parser.parse_args()
-        gui = gTrueCrypt()
-        gtk.main()
-        return 0
+		parser = OptionParser(version="%prog blub" )
+		(options, args) = parser.parse_args()
+		gui = gTrueCrypt()
+		gui.statusIcon()
+		gtk.main()
+		return 0
 
 if __name__ == "__main__":
 	print
 	print "###########################################################"
 	print "#                 gTrueCrypt 0.2-alpha                    #"
 	print "#                                                         #"
-	print "#   backend by:      Jens Kadenbach                       #"
-	print "#   gui by:       Sebastian Billaudelle                   #"
+	print "#   backend by:    Jens Kadenbach                         #"
+	print "#   gui by:     Sebastian Billaudelle                     #"
 	print "#                                                         #"
 	print "# This program is free software. It is published under    #"
 	print "# the terms of the of the GNU General Public License,     #"
@@ -306,4 +381,4 @@ if __name__ == "__main__":
 	print "# See the GNU General Public License for more details.    #"
 	print "###########################################################"
 	print
-        main()
+	main()
