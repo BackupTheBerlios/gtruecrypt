5da472d3fedd233c381778b8b5e9be4483bf2903
stein <stein>
2007-10-25 09:40:19 +0000

    basic languagefile ;-)

---
+++ truewrap/language_en.py
@@ -0,0 +1,7 @@
+language_en = {
+'help_new_general' : "Please select a valid path for your container.\nIf you want to create a hidden volume, select the path\nof the outer volume.\nIf you want to create a crypted partition,\nplease enter the devicepath!\n\nExamples:\n~/Crypt/cont.tc\n/dev/sda1",
+'help_new_voltype' : "Normaly you can see, if a file is a container or not.\nBut TrueCrypt also provides the 'Plausible Blablabla Concept',\nthat makes the container 'hidden'.\nBut notice: You have to 'hide' it in another container...",
+'help_new_filesystem' : "",
+'help_new_hash' : "",
+'help_new_enc' : ""
+}
